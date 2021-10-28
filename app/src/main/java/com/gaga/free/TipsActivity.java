6cf1ccbebe43b729ab116b0766ce54bb547dd972
gagawangzz <2356307010@qq.com>
2021-10-29 04:49:25 +0800

    Initial commit

---
+++ app/src/main/java/com/gaga/free/TipsActivity.java
@@ -0,0 +1,35 @@
+package com.gaga.free;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.gaga.free.adapter.TipsAdapter;
+import com.gaga.free.bean.DayWeatherBean;
+
+public class TipsActivity extends AppCompatActivity {
+
+    private RecyclerView rlvTips;
+    private TipsAdapter mTipsAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tips);
+
+        rlvTips = findViewById(R.id.rlv_tips);
+
+        Intent intent = getIntent();
+        DayWeatherBean weatherBean = (DayWeatherBean) intent.getSerializableExtra("tips");
+        if (weatherBean == null) {
+            return;
+        }
+        mTipsAdapter = new TipsAdapter(this,weatherBean.getTipsBeans());
+        rlvTips.setAdapter(mTipsAdapter);
+        rlvTips.setLayoutManager(new LinearLayoutManager(this));
+
+    }
+}
---
+++ app/src/main/java/com/gaga/free/bean/OtherTipsBean.java
@@ -0,0 +1,48 @@
+package com.gaga.free.bean;
+
+
+import com.google.gson.annotations.SerializedName;
+
+import java.io.Serializable;
+
+public class OtherTipsBean implements Serializable {
+
+    private String title;
+
+    private String level;
+
+    private String desc;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    @Override
+    public String toString() {
+        return "OtherTipsBean{" +
+                "title='" + title + '\'' +
+                ", level='" + level + '\'' +
+                ", desc='" + desc + '\'' +
+                '}';
+    }
+}
